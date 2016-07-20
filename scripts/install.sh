b938f0c662fee74f460a8e3bbcb8d4648c55c25d
Rudi Yardley <contact@rudiyardley.com>
2016-07-20 15:39:35 +0700

    pass correct env vars to remote server

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source ./scripts/ensure_remote_consistant.sh
 
+SSH_ENV_VARS="PROJECT=${PROJECT} REPOSITORY=${REPOSITORY} PORT=${PORT} DOMAIN=${DOMAIN}"
+
 echo 'Logging into server...'
-ssh $DEPLOY_USER@$DEPLOY_SERVER "PROJECT=${PROJECT} REPOSITORY=${REPOSITORY} bash -s" < ${DIR}/install_remote.sh
+ssh $DEPLOY_USER@$DEPLOY_SERVER "${SSH_ENV_VARS} bash -s" < ${DIR}/install_remote.sh
