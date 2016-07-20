e98761a1bada8a8f6f75ccbabc530b842790fb92
Rudi Yardley <contact@rudiyardley.com>
2016-07-20 15:34:46 +0700

    change to be sed dynamic templates

--- scripts/push_prod_remote.sh
+++ scripts/push_prod_remote.sh
@@ -5,9 +5,12 @@ git pull
 npm i --only=production
 npm run build
 chown -R ${PROJECT}:${PROJECT} ${WD}
-cp -f ${WD}/scripts/conf/nginx.conf /etc/nginx/sites-available/${PROJECT}.conf
+sed "s/%{PROJECT}/${PROJECT}/;s/%{PORT}/${PORT}/;s/%{DOMAIN}/${DOMAIN}/" ${WD}/scripts/conf/nginx.conf > /etc/nginx/sites-available/${PROJECT}.conf
+# cp -f ${WD}/scripts/conf/nginx.conf /etc/nginx/sites-available/${PROJECT}.conf
+
 ln -f -s /etc/nginx/sites-available/${PROJECT}.conf /etc/nginx/sites-enabled
-cp -f ${WD}/scripts/conf/upstart.conf /etc/init/${PROJECT}.conf
+# cp -f ${WD}/scripts/conf/upstart.conf /etc/init/${PROJECT}.conf
+sed "s/%{PROJECT}/${PROJECT}/;s/%{PORT}/${PORT}/;s/%{DOMAIN}/${DOMAIN}/" ${WD}/scripts/conf/upstart.conf > /etc/init/${PROJECT}.conf
 service ${PROJECT} restart
 service nginx restart
 exit
