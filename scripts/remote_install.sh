4e1784deb2c2be2d72cc1e754911198b29b2a104
Rudi Yardley <contact@rudiyardley.com>
2016-07-20 14:06:53 +0700

    use npm alias

--- scripts/remote_install.sh
+++ scripts/remote_install.sh
@@ -3,7 +3,7 @@ apt-get install -y git
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs build-essential nginx
 groupadd ${PROJECT}
-useradd -G ${PROJECT} ${PROJECT}
+useradd -g ${PROJECT} ${PROJECT}
 
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
 
