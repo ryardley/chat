a6c7c6e67b900f1f23ef8cfedc358d1d47db590d
Rudi Yardley <contact@rudiyardley.com>
2016-07-20 14:00:40 +0700

    create groups as part of installation process

--- scripts/remote_install.sh
+++ scripts/remote_install.sh
@@ -2,6 +2,8 @@ apt-get update
 apt-get install -y git
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs build-essential nginx
+groupadd ${PROJECT}
+useradd -G ${PROJECT} ${PROJECT}
 
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
 
